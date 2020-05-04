c7d7b2fcb886465d597e59a92a1673c069865e04
lordTocho <brian.highnam@accenturefederaldefense.com>
2020-05-04 14:41:33 -0400

    sa

--- push-for-lambda.ps1
+++ push-for-lambda.ps1
@@ -1,4 +1,4 @@
-Install-Module -Name AWSLambdaPSCore
+Install-Module -Name AWSLambdaPSCore -Force
 Import-Module AWSLambdaPSCore
 Write-Host "starting lambda function --> stop-instances"
 Publish-AWSPowerShellLambda -ScriptPath ./stop-instances-lambda/stop-instances.ps1 -Name stop-instances -Region us-east-1
